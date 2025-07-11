7e77e76e0c41dfd886a2da09c80815c315dba975
Jake Z <jakezcanada@gmail.com>
2025-07-11 16:02:33 -0400

    Login page base UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Dashboard } from './components/dashboard/dashboard';
 import { Timer } from './components/timer/timer';
 import { Settings } from './components/settings/settings';
 import { Statistics } from './components/statistics/statistics';
+import { Login } from './components/login/login';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'timer', component: Timer },
   { path: 'settings', component: Settings },
   { path: 'statistics', component: Statistics },
+  { path: 'login', component: Login },
   { path: '**', redirectTo: '/dashboard' }
 ];
---
+++ src/app/components/login/login.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    RouterLink,
+    FormsModule
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+
+}
