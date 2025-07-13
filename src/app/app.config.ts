b2c990f16394f721d3c9dc76d567272ca3249d57
DarrenLee <leex0320@mylaurier.ca>
2025-07-12 20:36:08 -0400

    Test Deployment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,7 +10,7 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
-import { firebaseConfig } from '../enviroments/environment';
+import { firebaseConfig } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
